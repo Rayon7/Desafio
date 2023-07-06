c0da424ae617e0d00403902ec07b2fc565bd5f8b
Leonardo Pedroso <leo.mendespedroso@gmail.com>
2023-07-06 01:05:44 -0300

    Menu and first task completed.

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 while True:
     print('Opções\n1 - Reverter a Ordem das Palavras.\n2 - Remover Caracteres Duplicados.')
-    print('3 - Encontrar Palindromo Mais Longo.\n4 - Colocar a Primeira Letra de Cada Frase Maiúscula')
+    print('3 - Encontrar Palindromo Mais Longo.\n4 - Colocar a Primeira Letra de Cada Frase Maiúscula.')
     print('5 - Verificar se Uma String é um Anagrama de Um Palindromo.\n6 - Sair.')
     op = input('Escolha a Opção Desejada: ')
-    if op.isdigit():
-        op = int(op)
+    if op.isdigit():  # Test if the operator texted is in fact a number.
+        op = int(op)  # Transform the number texted in an integer.
     if op == 6:
+        print('Programa Encerrado.')
         break
-
-
-
-
-
-
-    print('Insira uma frase para que sua ordem seja revertida: ')
-    fraseInput = input()
-    mix = fraseInput.split()
-    mix2 = mix[::-1]
-    fraseOutput = ' '.join(mix2)
-    print(fraseOutput)
+    elif op == 1:
+        fInput = input('Insira uma frase para que sua ordem seja revertida: ')
+        mix = fInput.split()  # Divide the string into substrings using an specified separator, in this case, blank spaces.
+        mix2 = mix[::-1]  # Revert the order of all words.
+        fOutput = ' '.join(mix2)  # Rearrange them and put the blank spaces back.
+        print(f'Frase Entrada: {fInput}\nFrase Saída: {fOutput}')
